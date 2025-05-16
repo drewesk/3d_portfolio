171bcadec2dae6747cc832f77489945d72bc3562
username <drewesk@gmail.com>
2025-05-16 19:37:10 -0400

    added hot reloading on localhost, some changes to Vite generated code

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    watch: {
+      usePolling: true
+    },
+    host: "localhost", // or '127.0.0.1'
+    port: 3000, // Optional: Specify a port number, default is 5173
+  },
+});
