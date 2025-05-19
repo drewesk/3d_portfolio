c8a5d33572ae039720a1ed5380f2bfe3c1c19472
username <drewesk@gmail.com>
2025-05-19 19:48:54 -0400

    adjusting 3d-Island dimensions and rotation effects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,27 @@ import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 import Island from '../models/Island'
+
       // <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
       //   POPUP
       // </div>
 
 const Home = () => {
+
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null; 
+    let screenPosition = [0, -6.5, -43];
+    let islandRotation = [0.1, 4.7, 0]
+
+    if(window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9]
+    } else {
+      screenScale = [1, 1, 1]
+  }
+  return [screenScale, screenPosition, islandRotation]
+}
+
+const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
   return (
     <section className='w-full h-screen relative'>
       <Canvas className='w-full h-screen bg-transparent' camera={{
@@ -14,12 +30,20 @@ const Home = () => {
         far: 1000,
       }}>
         <Suspense fallback={<Loader/>}>
-          <directionalLight/>
-          <ambientLight/>
-          <pointLight/>  
-          <spotLight/>
-          <hemisphereLight/>
-          <Island/>
+          <directionalLight position={[1,1,1]} intensity={2}/>
+          <ambientLight intensity={0.5}/>
+          <hemisphereLight skyColor="#b1e1ff" groundColor="#000"/>
+          <Island
+            position={
+              islandPosition
+            }
+            scale={
+              islandScale
+            }
+            rotation={
+              islandRotation
+            }
+          />
         </Suspense>
       </Canvas>
     </section>
